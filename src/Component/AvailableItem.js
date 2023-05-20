104e30f6a89669480aadae97f99b9abc4da2abda
asif-karim100 <asif.karim100@gmail.com>
2023-05-20 20:22:45 +0530

    first commit

---
+++ src/Component/AvailableItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const AvailableItem = ({products}) => {
+
+  return <div>
+      {products.map(product => (
+        <div key={product.title}>
+           <h3>{product.title}</h3>    
+           <img src={product.imageUrl} alt="no " className=""></img>
+           <p> Price: {product.price}</p>
+        </div>
+      )) }
+  </div>;
+};
+
+export default AvailableItem;
