d5d5900f33f30ed5faa7cda071d1a52b75f9e1b7
asif-karim100 <113389378+asif-karim100@users.noreply.github.com>
2023-06-20 01:36:42 +0530

    Signup and Authentication

--- src/App.js
+++ src/App.js
@@ -1,78 +1,25 @@
-import {useState} from "react";
-import "./App.css";
-import Header from "./components/LayOut/Header";
-// import ProductList from "./components/ProductList";
-import CartList from "./components/CartList";
-// import { Cart } from "./components/CartContext";
-import { Route,Routes } from "react-router-dom";
-import Home from "./components/Pages/Home";
-import About from "./components/Pages/About";
-import Store from "./components/Pages/Store";
-import ContactUs from "./components/Pages/ContactUs";
-import ProductDetails from "./components/Pages/productDetails";
+import { Switch, Route } from 'react-router-dom';
 
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
-// import Cartpro from "./components/Cart/Cartpro";
-
-
-const App = ()  => {
-  const [showCart,setShowCart] = useState(false);
-
-  // const { cart, addToCart} = useContext(Cart);
-
-
-
-
-const handleShow = (value) => {
-  setShowCart(value)
-};
-
-// {
-//   showCart ? 
-//   <CartList/> :
-
-// }
-// {
-//   showCart ? 
-//   <CartList/> :
-//   <ProductList addToCart={addToCart}/>
-
-// }
-
-//  <BrowserRouter>
-// <Routes>
-// <Route path="/home" element={<Home />} />
-// <Route path="store" element={showCart ? <CartList /> : <Store />} />
-// <Route path="about" element={<About />} />
-
-
-// </Routes>
-
-// </BrowserRouter>
-
-
+function App() {
   return (
-    <div>
-   
-    
-<Header  handleShow={handleShow}/>
-
-      
-
-
- <Routes>
- <Route path="/home" element={<Home />} />
- <Route path="store" element={showCart ? <CartList /> : <Store />} />
- <Route path="about" element={<About />} />
- <Route path="contact" element={<ContactUs></ContactUs>}></Route>
- <Route path="/product/:id" element={<ProductDetails/>} /> 
-
-
- </Routes>
-
-    
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import ReactDOM from "react-dom/client";
-import "../node_modules/react-bootstrap/dist/react-bootstrap";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-// import './index.css';
-import CartContext from './components/CartContext';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 root.render(
-<BrowserRouter>
-    <CartContext>
-        <App />
-    </CartContext>
-    </BrowserRouter>
-   
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
